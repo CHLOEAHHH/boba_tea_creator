347235df7038df9b20698ac6ed0362c8752b6a2c
WDAGUtilityAccount <WDAGUtilityAccount@cef8248b-80bd-4707-91a5-34c05eec76a2.mshome.net>
2023-12-14 11:02:12 -0600

    First commitment

---
+++ src/main/java/com/whhs/boba_tea_creator/SystemInfo.java
@@ -0,0 +1,13 @@
+package com.whhs.boba_tea_creator;
+
+public class SystemInfo {
+
+    public static String javaVersion() {
+        return System.getProperty("java.version");
+    }
+
+    public static String javafxVersion() {
+        return System.getProperty("javafx.version");
+    }
+
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,4 @@
+module com.whhs.boba_tea_creator {
+    requires javafx.controls;
+    exports com.whhs.boba_tea_creator;
+}
